c9cfcccb0bbf829dd92609807a12927bfd25c685
Darionti88 <darionti@gmail.com>
2021-11-08 15:39:38 -0300

    Partial Design Edit_Account

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,9 +2,11 @@ import React from "react";
 import { useSession, signOut } from "next-auth/client";
 import Link from "next/link";
 import { Button } from "@chakra-ui/button";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
   const [session] = useSession();
+  const router = useRouter();
 
   return (
     <nav className='flex justify-between items-center px-10 py-3 sticky z-50 top-0 bg-white shadow-md rounded-br-lg rounded-bl-lg'>
@@ -14,21 +16,27 @@ const Navbar = () => {
         </Link>
       </div>
       <ul className='flex flex-1 items-center space-x-20 justify-end text-3xl pr-10'>
-        <li>
-          <Link href='/profile' className='font-bold  '>
-            Profile
-          </Link>
-        </li>
-        <li>
-          <Link href='/profile' className='font-bold '>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link href='/profile' className='font-bold '>
-            Contact
-          </Link>
-        </li>
+        {session && (
+          <li>
+            <Link href='/edit_account' className='font-bold  '>
+              Profile
+            </Link>
+          </li>
+        )}
+        {router.route === "/" && (
+          <>
+            <li>
+              <Link href='/profile' className='font-bold '>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href='/profile' className='font-bold '>
+                Contact
+              </Link>
+            </li>
+          </>
+        )}
 
         {!session && (
           <li>
@@ -43,7 +51,7 @@ const Navbar = () => {
               size='lg'
               onClick={() => signOut()}
               textColor='#FFF'
-              colorScheme='blackAlpha'>
+              colorScheme='purple'>
               Sign Out
             </Button>
           </li>
