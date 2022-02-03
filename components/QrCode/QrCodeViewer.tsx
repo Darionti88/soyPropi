f5d6a537d908cf24c76e7041ac5f49fccd2c5972
Darionti88 <darionti@gmail.com>
2022-02-03 18:05:01 -0300

    add export QR to PDF

--- components/QrCode/QrCodeViewer.tsx
+++ components/QrCode/QrCodeViewer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import QRcode from "./QRcode";
+import { jsPDF } from "jspdf";
 import { ArrowBackIcon, DownloadIcon } from "@chakra-ui/icons";
 import { Button } from "@chakra-ui/react";
 import { QRProps } from "../../types/types";
@@ -7,33 +8,54 @@ import { QRProps } from "../../types/types";
 const QrCodeViewer: React.FC<QRProps> = ({
   setImageUrl,
   imageUrl,
-  handleSaveQrCode,
-}) => (
-  <div className='flex flex-col w-full jusify-center items-center'>
-    <div className='flex flex-row space-x-10 py-10 w-full'>
-      <Button
-        leftIcon={<ArrowBackIcon />}
-        colorScheme='messenger'
-        size='lg'
-        width='50%'
-        variant='solid'
-        onClick={() => setImageUrl("")}>
-        Volver
-      </Button>
-      <Button
-        leftIcon={<DownloadIcon />}
-        colorScheme='messenger'
-        size='lg'
-        width='50%'
-        variant='solid'
-        onClick={() => handleSaveQrCode}>
-        Guardar
-      </Button>
+  profileName,
+}) => {
+  const handleSaveQrCode = () => {
+    const doc = new jsPDF("p", "pt", "a4");
+    doc.setFontSize(30);
+    doc.html(document.getElementById("qrcode"), {
+      callback: function (doc) {
+        doc.save(`propi${profileName}.pdf`);
+      },
+      x: -25,
+    });
+  };
+
+  return (
+    <div className='flex flex-row w-full jusify-center items-center'>
+      <div className='flex flex-col space-y-10  w-1/4'>
+        <Button
+          leftIcon={<ArrowBackIcon />}
+          colorScheme='messenger'
+          size='lg'
+          width='100%'
+          variant='solid'
+          onClick={() => setImageUrl("")}>
+          Volver
+        </Button>
+        <Button
+          leftIcon={<DownloadIcon />}
+          colorScheme='messenger'
+          size='lg'
+          width='100%'
+          variant='solid'
+          onClick={() => handleSaveQrCode()}>
+          Guardar
+        </Button>
+      </div>
+      <div
+        id='qrcode'
+        className='flex flex-col text-5xl h-full w-3/4 items-center justify-center'>
+        <p className='text-4xl text-primary-mpago700 mb-2 font-exo2 font-bold'>
+          Propi
+        </p>
+        <p className='text-2xl mb-7'>
+          Escanea el Código QR y elegí el monto de la propina!
+        </p>
+        <QRcode imageUrl={imageUrl} />
+      </div>
     </div>
-    <div className='flex w-full items-center justify-center '>
-      <QRcode imageUrl={imageUrl} />
-    </div>
-  </div>
-);
+  );
+};
 
 export default QrCodeViewer;
